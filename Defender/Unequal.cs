1554e9b3ff2268e86cdf3d7f33f1d8ae24dff29a
Patrick Kelly <p-kell@live.com>
2020-01-25 14:53:15 -0500

    Implemented basic Unequal() for arguments

--- Defender/Unequal.cs
+++ Defender/Unequal.cs
@@ -4,21 +4,21 @@ using System.Runtime.CompilerServices;
 namespace Defender {
 	public static partial class Guard {
 		/// <summary>
-		/// Guard against the argument being unequal to the specified value.
+		/// Guard against the argument being equal to the specified value.
 		/// </summary>
 		/// <typeparam name="T">The type of the argument.</typeparam>
 		/// <param name="value">The value.</param>
 		/// <param name="name">The name of the argument.</param>
 		/// <param name="other">The value <paramref name="value"/> must be equal to.</param>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static void Equal<T>(T value, String name, T other) {
-			if (!Equals(value, other)) {
-				throw new ArgumentInequalException(name, $"Argument must equal {other}.");
+		public static void Unequal<T>(T value, String name, T other) {
+			if (Equals(value, other)) {
+				throw new ArgumentEqualException(name, $"Argument must not equal {other}.");
 			}
 		}
 
 		/// <summary>
-		/// Guard against the <paramref name="first"/> argument being unequal to the <paramref name="second"/> argument.
+		/// Guard against the <paramref name="first"/> argument being equal to the <paramref name="second"/> argument.
 		/// </summary>
 		/// <typeparam name="T">The type of the argument.</typeparam>
 		/// <param name="first">The first value.</param>
@@ -26,9 +26,9 @@ namespace Defender {
 		/// <param name="second">The second value.</param>
 		/// <param name="secondName">The name of the second argument.</param>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static void Equal<T>(T first, String firstName, T second, String secondName) {
-			if (!Equals(first, second)) {
-				throw new ArgumentInequalException(firstName, $"Argument must equal {secondName}'s value of {second}.");
+		public static void Unequal<T>(T first, String firstName, T second, String secondName) {
+			if (Equals(first, second)) {
+				throw new ArgumentEqualException(firstName, $"Argument must not equal {secondName}'s value of {second}.");
 			}
 		}
 	}
