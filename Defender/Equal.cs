82b8be629d9aa16695523587e26454737f5d2ac1
Patrick Kelly <p-kell@live.com>
2020-02-10 22:01:15 -0500

    Added equal/unequal variants for array/collections

--- Defender/Equal.cs
+++ Defender/Equal.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
 namespace Defender {
@@ -29,6 +31,49 @@ namespace Defender {
 		public static void Equal<T>(T first, String firstName, T second, String secondName) {
 			if (!Equals(first, second)) {
 				throw new ArgumentInequalException(firstName, $"Argument must equal {secondName}'s value of {second}.");
+			}
+		}
+
+		/// <summary>
+		/// Guard against the <paramref name="array"/> being of different size from <paramref name="size"/>.
+		/// </summary>
+		/// <typeparam name="T">The type of the elements of the array.</typeparam>
+		/// <param name="array">The array.</param>
+		/// <param name="name">The name of the argument.</param>
+		/// <param name="size">The required size.</param>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void Equal<T>(T[] array, String name, Int32 size) {
+			if (array.Length != size) {
+				throw new ArgumentSizeException(name, $"Argument must contain '{size}' elements.");
+			}
+		}
+
+		/// <summary>
+		/// Guard against the <paramref name="collection"/> being of different size from <paramref name="size"/>.
+		/// </summary>
+		/// <typeparam name="C">The type of the argument; must be <see cref="ICollection"/>.</typeparam>
+		/// <param name="collection">The collection.</param>
+		/// <param name="name">The name of the argument.</param>
+		/// <param name="size">The required size.</param>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void Equal<C>(C collection, String name, Int32 size) where C : ICollection {
+			if (collection.Count != size) {
+				throw new ArgumentSizeException(name, $"Argument must contain '{size}' elements.");
+			}
+		}
+
+		/// <summary>
+		/// Guard against the <paramref name="collection"/> being of different size from <paramref name="size"/>.
+		/// </summary>
+		/// <typeparam name="C">The type of the argument; must be <see cref="ICollection{T}"/> of <typeparamref name="T"/>.</typeparam>
+		/// <typeparam name="T">The type of the elements of <typeparamref name="C"/>.</typeparam>
+		/// <param name="collection">The collection.</param>
+		/// <param name="name">The name of the argument.</param>
+		/// <param name="size">The required size.</param>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void Equal<C, T>(C collection, String name, Int32 size) where C : ICollection<T> {
+			if (collection.Count != size) {
+				throw new ArgumentSizeException(name, $"Argument must contain '{size}' elements.");
 			}
 		}
 	}
