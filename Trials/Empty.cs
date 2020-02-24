3c681959db3d9cf8aaa09b36f027b5d9c42dc32d
Patrick Kelly <p-kell@live.com>
2020-02-23 21:37:32 -0500

    Added Empty claims

---
+++ Trials/Empty.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Xunit.Sdk;
+
+namespace Defender {
+	public static partial class ClaimExtensions {
+		public static Claim<String> Empty(this Claim<String> claim) {
+			if (claim.Value.Length != 0) {
+				throw new EmptyException(claim.Value);
+			}
+			return claim;
+		}
+
+		public static Claim<T[]> Empty<T>(this Claim<T[]> claim) {
+			if (claim.Value.Length != 0) {
+				throw new EmptyException(claim.Value);
+			}
+			return claim;
+		}
+
+		public static Claim<C> Empty<C>(this Claim<C> claim) where C : ICollection {
+			if (claim.Value.Count != 0) {
+				throw new EmptyException(claim.Value);
+			}
+			return claim;
+		}
+
+		public static Claim<C> Empty<C, T>(this Claim<C> claim) where C : ICollection<T> {
+			if (claim.Value.Count != 0) {
+				throw new EmptyException(claim.Value);
+			}
+			return claim;
+		}
+	}
+}
